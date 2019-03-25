ea48036ae1539460a8faff64626b566308dd998d
kudzurunner <sdio@tuta.io>
2019-03-25 18:49:56 +0300

    added build with flag with_cuda=False

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
 from cpt.packager import ConanMultiPackager
+import os
 
+def disable_cuda(build):
+    build.options.update({'opencv:with_cuda': False})
+    return build
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="kudzurunner")
     builder.add_common_builds()
+    if 'CONAN_VISUAL_VERSIONS' in os.environ:
+        if os.environ['CONAN_VISUAL_VERSIONS'] == '15':
+            builder.builds = map(disable_cuda, builder.items)
     builder.run()
